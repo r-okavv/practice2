18e8ef8c43d6a062a9ab2da86c775cf2de22cb3b
r-okavv <101883340+r-okavv@users.noreply.github.com>
2022-08-26 00:24:27 +0900

    add FaceShowButton

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShow = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
 
   return (
     <>
@@ -13,7 +19,10 @@ const App = () => {
       <ColorfulMessage color="grey">How are you?</ColorfulMessage>
       <ColorfulMessage color="pink">I'm Fine!</ColorfulMessage>
       <button onClick={onClickCountUp}>Count up</button>
+      <br />
+      <button onClick={onClickSwitchShow}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>:)</p>}
     </>
   );
 };
